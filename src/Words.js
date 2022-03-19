543c396aab2ef4ec7dbb8358b4c9c6a5ad5c47ea
harrionparrix <varunpat44@gmail.com>
2022-03-19 08:45:56 +0530

    First Commit

---
+++ src/Words.js
@@ -0,0 +1,23 @@
+import WordBank from "./Wordle-bank.txt";
+
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  await fetch(WordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+      wordSet = new Set(wordArr);
+    });
+  return { wordSet, todaysWord };
+};
